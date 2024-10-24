a9417a2a44236bc9af59cd14c4cc9f849532f93a
rodolphe josé TETEVI <110821426+josetetevi95@users.noreply.github.com>
2024-10-24 10:11:58 +0200

    [FEATURE] : Add online mongo db connect and test API

--- app.js
+++ app.js
@@ -7,27 +7,27 @@ const productRoutes = require('./routes/productRoutes');
 dotenv.config();
 const app = express();
 
-// Middleware pour activer CORS sur localhost:3000
+// Middleware pour activer CORS sur localhost:ClientPort 
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: `http://localhost:${process.env.CLIENT_PORT}`
 }));
 
 // Middleware pour parser les requêtes JSON
 app.use(express.json());
 
 // Connexion à MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('Connecté à MongoDB');
-}).catch(err => {
-    console.error('Erreur de connexion à MongoDB:', err);
-});
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connecté à MongoDB');
+    })
+    .catch(err => {
+        console.error('Erreur de connexion à MongoDB:', err);
+    });
 
 // Utilisation des routes produits
 app.use('/api', productRoutes);
 
+// Démarrage du serveur sur le port spécifié
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur le port ${PORT}`);
